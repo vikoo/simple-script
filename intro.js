fc9d33a4950d037218b4973764d120f013de9d85
vikoo <vikoo.vikoo90@gmail.com>
2025-05-29 11:17:47 +0530

    initial commit

---
+++ intro.js
@@ -0,0 +1,11 @@
+<script>
+  window.inlineJsWorks = true;
+</script>
+
+<script>
+  if (!window.inlineJsWorks) {
+    console.warn("🚫 Inline JavaScript is blocked.");
+  } else {
+    console.log("✅ Inline JavaScript is allowed.");
+  }
+</script>
